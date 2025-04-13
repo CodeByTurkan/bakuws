f27110e00eea8154e63b25d3017bffa1302a2b7b
CodeByTurkan <turkantech@gmail.com>
2025-04-13 16:56:00 +0400

    first commit

---
+++ admin.js
@@ -0,0 +1,19 @@
+const body = document.querySelector('body')
+
+    // sehive yuklenen anda onload deyir, sildikki gorunmesin back side
+    let token = localStorage.getItem('token')
+    if (!token) {
+        body.style.display = 'none'
+        location.href = 'login.htm'
+    }
+
+    // token 3 hisseden ibaretdir.,, user info , 
+
+
+    // new project - news  - id, creaat, title img
+
+
+    // script js de fetch get ele. 
+
+    // item.text  - ""  -icerisnde cagir. 
+
---
+++ script.js
@@ -0,0 +1,27 @@
+const cards = document.getElementById('cards')
+
+async function getCards() {
+    const res = await fetch(" https://67fba7941f8b41c816847cf0.mockapi.io/bakuws")
+    const data = await res.json()
+    showCards(data)
+    
+}
+
+function showCards(data) {
+    let code = ''
+    data.map(elm => {
+        code +=
+        `<div class="max-w-sm  bg-white border border-gray-200 rounded-lg shadow-sm text-black">
+                <a href="" id=${elm.id}>
+                    <img class="rounded-t-lg h-50 object-cover w-full" src=${elm.image} alt=""/>
+                </a>
+                <div class="p-2">
+                    <p class="font-bold text-[20px]">${elm.title}</p>
+                </div>
+            </div>
+        `
+        
+    })
+    cards.innerHTML = code    //you call cards after they have been completed not before
+}
+getCards()
